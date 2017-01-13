cd42a23b7116bb5654e11856b331f5b51e74cc0e
기호 <david9921@naver.com>
2017-01-13 14:11:21 +0900

    Variable Example

---
+++ VariableExample/src/com/test/variable_exam/Variable_exam.java
@@ -0,0 +1,9 @@
+package com.test.variable_exam;
+
+public class Variable_exam {
+	public static void main(String[] args){
+		int value =18;
+		int result = value+10;
+		System.out.println(result);
+	}
+}
